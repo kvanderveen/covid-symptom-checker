1222061d67020b6fff3ab3fff34bb5d50bfe41c9
Kevin Vanderveen <kevin.r.vanderveen@gmail.com>
2020-07-16 20:19:39 -0600

    improved functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,72 +1,75 @@
 const start = document.querySelector('.start');
 const question = document.querySelector('.question');
-const symptoms1 = document.querySelector('.symptoms-1');
-const symptoms2 = document.querySelector('.symptoms-2');
-const symptoms3 = document.querySelector('.symptoms-3');
-const testing = document.querySelector('.testing');
-const noSymptoms = document.querySelector('.no-symptoms');
+const form = document.querySelector('.form');
+const recommendation = document.querySelector('.recommendation');
 
-
-// start questions with symptom question
-start.addEventListener('click', () => {
-    start.remove();
-    question.textContent = 'Are you currently having symptoms?';
-    symptoms1.innerHTML = questionOneHTML;
-    question2();
-});
-
-// proceed to question 2
-const question2 = () => {
-    symptoms1.addEventListener('submit', e => {
-        e.preventDefault();
-        const answer1 = symptoms1.question1.value;
-        if (answer1 === 'yes') {
-            symptoms1.remove();
-            question.textContent = 'Please include the symptoms you are experiencing.';
-            symptoms2.innerHTML = questionTwoHTML;
-            question3();
-        } else {
-            question.remove();
-            symptoms1.remove();
-            noSymptoms.innerHTML = noSymptomsHTML;
-        }
-    })
-};
-
-// proceed to question 3
-const question3 = () => {
-    symptoms2.addEventListener('submit', e => {
-        e.preventDefault();
-        const answers = symptoms2.querySelectorAll('.question2');
-        answers.forEach(answer => {
-            if (answer.checked & answer.value !== 'none') {
-                question.remove();
-                symptoms2.remove();
-                testingRecommended();
-                return false;
-            }
-        })
-        symptoms2.remove();
-        question.textContent = 'Please check any of the following symptoms you are experiencing.';
-        symptoms3.innerHTML = questionThreeHTML;
-        question4();
-    })
+function startQuestions() {
+    start.addEventListener('click', () => {
+        start.remove();
+        question.textContent = 'Are you having any symptoms?';
+        form.innerHTML = symptomsYesOrNo;
+        funcYesOrNo();
+    });
 }
 
-// proceed to question 4
-const question4 = () => {
-    symptoms3.addEventListener('submit', e => {
+function funcYesOrNo() {
+    const formYesOrNo = document.querySelector('.yes-or-no');
+    formYesOrNo.addEventListener('submit', e => {
         e.preventDefault();
-        const answers = symptoms3.querySelectorAll('.question3');
+        if (formYesOrNo.question.value === 'no') {
+            question.remove();
+            form.remove();
+            recommendation.innerHTML = noSymptomsHTML;
+        } else {
+            question.textContent = 'Check any of the following symptoms you are having.';
+            form.innerHTML = majorSymptoms;
+            funcMajorSymptoms();
+        }
+    });
+}
+
+function funcMajorSymptoms() {
+    const formMajorSymptoms = document.querySelector('.major-symptoms');
+    formMajorSymptoms.addEventListener('submit', e => {
+        e.preventDefault();
+        const answers = formMajorSymptoms.querySelectorAll('input');
         let total = 0;
         answers.forEach(answer => {
-            if (answer.checked && answer.value !== 'none') { total++ }
+            if (answer.checked & answer.value !== 'none') {
+                total++;
+            }
         });
-        console.log(total);
+        if (total > 0){
+            question.remove();
+            form.remove();
+            recommendation.innerHTML = symptomsHTML;
+        } else {
+            form.innerHTML = minorSymptoms;
+            funcMinorSymptoms();
+        }
     })
 }
 
-// testing recommended
-const testingRecommended = () => {
-    testing.innerHTML = 'testing is recommended!'
+function funcMinorSymptoms() {
+    const formMinorSymptoms = document.querySelector('.minor-symptoms');
+    formMinorSymptoms.addEventListener('submit', e => {
+        e.preventDefault();
+        const answers = formMinorSymptoms.querySelectorAll('input');
+        let total = 0;
+        answers.forEach(answer => {
+            if (answer.checked & answer.value !== 'none') {
+                total++;
+            }
+        });
+        question.remove();
+        form.remove();
+        if (total > 1){
+            recommendation.innerHTML = symptomsHTML;
+        } else {
+            form.innerHTML = minorSymptoms;
+            recommendation.innerHTML = noSymptomsHTML;
+        }
+    })
 }
+
+startQuestions();
--- scripts/questions.js
+++ scripts/questions.js
@@ -1,106 +1,121 @@
-questionOneHTML = `
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="question-1-yes" value="yes" checked>
-        <label class="form-check-label" for="question-1-yes">
-            Yes
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="question-1-no" value="no">
-        <label class="form-check-label" for="question-1-no">
-            No
-        </label>
-    </div>
-    <button class="question-1 btn btn-primary btn-lg my-3" type="submit">Submit</button>
+symptomsYesOrNo = `
+    <form class="yes-or-no">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="question" id="question-1-yes" value="yes" checked>
+            <label class="form-check-label" for="question-1-yes">
+                Yes
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="question" id="question-1-no" value="no">
+            <label class="form-check-label" for="question-1-no">
+                No
+            </label>
+        </div>
+        <button class="btn btn-primary btn-lg my-3" type="submit">Submit</button>
+    </form>    
 `;
 
-questionTwoHTML = `
-    <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="fever" id="fever">
-        <label class="form-check-label" for="fever">
-            fever over 100&deg;F or chills
-        </label>
-    </div>
+majorSymptoms = `
+    <form class="major-symptoms">
         <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="cough" id="cough">
-        <label class="form-check-label" for="cough">
-            cough
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="sob" id="sob">
-        <label class="form-check-label" for="sob">
-            shortness of breath or difficulty breathing
-        </label>
-    </div>
+            <input class="form-check-input" type="checkbox" value="fever" id="fever">
+            <label class="form-check-label" for="fever">
+                fever over 100&deg;F or chills
+            </label>
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="cough" id="cough">
+            <label class="form-check-label" for="cough">
+                cough
+            </label>
+        </div>
         <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="smell" id="smell">
-        <label class="form-check-label" for="smell">
-            new loss of taste or smell
-        </label>
-    </div> 
-    </div>
-        <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="none" id="none">
-        <label class="form-check-label" for="none">
-            none of the above
-        </label>
-    </div> 
-    <button class="question-1 btn btn-primary btn-lg my-3" type="submit">Submit</button>   
+            <input class="form-check-input" type="checkbox" value="sob" id="sob">
+            <label class="form-check-label" for="sob">
+                shortness of breath or difficulty breathing
+            </label>
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="smell" id="smell">
+            <label class="form-check-label" for="smell">
+                new loss of taste or smell
+            </label>
+        </div> 
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="none" id="none">
+            <label class="form-check-label" for="none">
+                none of the above
+            </label>
+        </div> 
+        <button class="question-1 btn btn-primary btn-lg my-3" type="submit">Submit</button>
+    </form>       
 `;
 
-questionThreeHTML = `
-    <div class="form-check">
-        <input class="form-check-input question3" type="checkbox" value="fatigue" id="fatigue">
-        <label class="form-check-label" for="fatigue">
-            severe fatigue
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input question3" type="checkbox" value="aches" id="aches">
-        <label class="form-check-label" for="aches">
-            muscle or body aches
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input question3" type="checkbox" value="headache" id="headache">
-        <label class="form-check-label" for="headache">
-            headache
-        </label>
-    </div>
+minorSymptoms = `
+    <form class="minor-symptoms">
         <div class="form-check">
-        <input class="form-check-input question3" type="checkbox" value="sore-throat" id="sore-throat">
-        <label class="form-check-label" for="sore-throat">
-            sore throat
-        </label>
-    </div> 
-    </div>
+            <input class="form-check-input" type="checkbox" value="fatigue" id="fatigue">
+            <label class="form-check-label" for="fatigue">
+                severe fatigue
+            </label>
+        </div>
         <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="congestion" id="congestion">
-        <label class="form-check-label" for="congestion">
-            nasal congestion or a runny nose
-        </label>
-    </div> 
-    </div>
+            <input class="form-check-input" type="checkbox" value="aches" id="aches">
+            <label class="form-check-label" for="aches">
+                muscle or body aches
+            </label>
+        </div>
         <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="vomiting" id="vomiting">
-        <label class="form-check-label" for="vomiting">
-            vomiting or diarrhea
-        </label>
-    </div>
-    </div>
-        <div class="form-check">
-        <input class="form-check-input question2" type="checkbox" value="none" id="none">
-        <label class="form-check-label" for="none">
-            none of the above
-        </label>
-    </div>
-    <button class="question-1 btn btn-primary btn-lg my-3" type="submit">Submit</button>   
+            <input class="form-check-input" type="checkbox" value="headache" id="headache">
+            <label class="form-check-label" for="headache">
+                headache
+            </label>
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="sore-throat" id="sore-throat">
+            <label class="form-check-label" for="sore-throat">
+                sore throat
+            </label>
+        </div> 
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="congestion" id="congestion">
+            <label class="form-check-label" for="congestion">
+                nasal congestion or a runny nose
+            </label>
+        </div> 
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="vomiting" id="vomiting">
+            <label class="form-check-label" for="vomiting">
+                vomiting or diarrhea
+            </label>
+        </div>
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="none" id="none">
+            <label class="form-check-label" for="none">
+                none of the above
+            </label>
+        </div>
+        <button class="question-1 btn btn-primary btn-lg my-3" type="submit">Submit</button> 
+    </form>      
 `;
 
 noSymptomsHTML = `
-    In the absence of symptoms, testing is not currently recommended.  If you believe you have
-    come in close contact with someone with a COVID-19 infection, then you should 
-    <a href="https://covid19.colorado.gov/how-to-quarantine" target="_blank">quarantine</a> 
-    for 14 days from the last date of contact with that individual.
+    In the absence of symptoms or symptoms suggestive of covid-19, testing is 
+    not currently recommended.  If you believe you have come in close contact
+    with someone with a COVID-19 infection, then you should 
+    <a href="https://covid19.colorado.gov/how-to-quarantine" target="_blank">
+    quarantine</a> for 14 days from the last date of contact with that 
+    individual.
+`;
+
+symptomsHTML = `
+    Based upon your reported symptoms, testing is recommended.  Testing is 
+    currenty being performed at these 
+    <a href="https://search.211colorado.org/search?terms=COVID-19%20Testing&page=1&location=Colorado&service_area=colorado" target="_blank">
+    sites</a>.
 `
